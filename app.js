8ee8bcd545fd64f32c539a5c399c8fb0a098c797
Alan Nishioka <alan@nishioka.com>
2026-04-20 13:05:16 -0700

    moved trains.js into app.js.  works!

--- app.js
+++ app.js
@@ -1,8 +1,88 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
+const { fetchStation } = require("amtrak");
+const { fetchTrain }   = require("amtrak");
 
-app.get("/", (req, res) => res.type('html').send(html));
+const lerp = (start, end, amt) => start + amt * (end - start);
+
+app.get("/", (req, res) =>
+{
+  let html = '<!DOCTYPE html><html><body>';
+  let trainArray = [];
+
+  fetchStation("EMY").then((station) => {
+    // console.log(station);
+    // console.log(station.EMY.trains);
+
+    let promises = []
+
+    // Add now to train array
+    trainArray.push( { stamp: new Date(), name: '\t\t----- NOW -----' } );
+
+    station.EMY.trains.forEach((train_number) => {
+
+      promises.push( fetchTrain(train_number).then((train) => {
+        // console.log(Object.values(train)[0][0].routeName);
+        let x = Object.values(train)[0][0];
+
+        // if( x.trainNum == 710 ) console.log( x );
+
+        let station = '';
+        let nextStation = '';
+        let stations = x.stations;
+        for( let i=0; i < stations.length; i++ )
+        {
+          // Just save MTZ and EMY
+          if(( stations[i].code == 'EMY' ) || ( stations[i].code == 'MTZ' ))
+          {
+            if( station == '' ) station = stations[i];
+            else if( nextStation == '' ) nextStation = stations[i];
+          }
+        }
+
+        let dep = Date.parse( station.dep );
+        let arr = Date.parse( nextStation.arr );
+        let mid;
+
+        // If arr/dep null, use schArr/schDep instead
+        if( station.dep     == null ) dep = Date.parse( station.schDep );
+        if( nextStation.arr == null ) arr = Date.parse( station.schArr );
+
+        if( station.code == 'EMY' )
+        {
+          mid = new Date( lerp( dep, arr, 3.7 / 34 ));
+        }
+        else
+        {
+          mid = new Date( lerp( arr, dep, 3.7 / 34 ));
+        }
+
+        // console.log( station.code, station.dep );
+        // console.log( nextStation.code, nextStation.arr );
+        // console.log( 'BUC', mid.toLocaleTimeString());
+
+        // console.log( x.trainNum + '\t' + x.routeName + '\t' + mid.toLocaleTimeString());
+        trainArray.push( { stamp: mid, name: x.trainNum + '\t' + x.destCode + '\t' + x.routeName } );
+      }));
+
+    });
+
+    return Promise.all(promises);
+
+  }).then(() => {
+    trainArray.sort((a, b) => a.stamp.getTime() - b.stamp.getTime());
+
+    for( let i = 0; i < trainArray.length; i++ )
+    {
+      //console.log( trainArray[i].stamp.toLocaleTimeString(), trainArray[i].name );
+      console.log( trainArray[i].stamp.toLocaleString() +'\t' + trainArray[i].name );
+      html += trainArray[i].stamp.toLocaleString() +'\t' + trainArray[i].name + '<br>';
+    }
+
+    res.type('html').send(html)
+  });
+});
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 

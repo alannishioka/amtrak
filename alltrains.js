83def64273053dd3d4fafa68626aa6046af7e141
Alan Nishioka <alan@nishioka.com>
2026-04-22 05:46:25 -0700

    loops thru all trains

--- alltrains.js
+++ alltrains.js
@@ -2,5 +2,14 @@ const { fetchAllTrains } = require("amtrak"); // CommonJS
 
 // JS
 fetchAllTrains().then((trains) => {
-  console.log(trains);
+  // console.log(trains);
+  let trainsArray = Object.values(trains);
+
+  for( let i = 0; i < trainsArray.length; i++ )
+  {
+    for( let j = 0; j < trainsArray[i].length; j++ )
+    {
+      console.log( trainsArray[i][j].trainID, trainsArray[i][j].routeName );
+    }
+  }
 });
